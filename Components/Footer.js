8336a4b1e1250b99ffe9061fcce527d68e8c7a3a
Lloyd-the-god <101338048+Lloyd-the-god@users.noreply.github.com>
2022-04-21 14:33:22 +0100

    Add files via upload

---
+++ Components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./Footer.css"
+
+
+ function footer() {
+    return <>
+    <footer>
+      <p>Copyright &copy; My Restaurant 2022</p>
+    </footer>
+    </>
+ }
+
+
+
+export default footer
